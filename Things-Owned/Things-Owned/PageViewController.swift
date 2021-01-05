89ddffd01e5e1e76dacd866ef3c07ea800518a79
Test <Test>
2021-01-05 13:06:23 -0600

    fjhgasgkjd

--- Things-Owned/Things-Owned/PageViewController.swift
+++ Things-Owned/Things-Owned/PageViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class PreviewViewController: UIPageViewController, UIPageViewControllerDataSource, MainVCDelegate {
     
+    //Test
+    
     var currentPage = 0;
     
     lazy var viewControllerList:[UIViewController] = {
